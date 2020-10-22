1bbf14e35eac66e2fce0301052fc731c53ae8476
Sakshi <66660153+Sakshiiii23@users.noreply.github.com>
2020-10-22 22:26:30 +0530

    Update smallerNumbersThanCurrent.cpp

--- smallerNumbersThanCurrent.cpp
+++ smallerNumbersThanCurrent.cpp
@@ -1,5 +1,5 @@
-Q.Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
-  That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+Q.Given the array nums, fr each nums[i] find out how many numbers in the array are smaller than it.
+  That is, fr each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 
 class Solution {
 public:
