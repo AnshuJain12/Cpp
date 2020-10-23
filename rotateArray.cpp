e9dc05c4b608de2c27776afe417208b3694f8c09
Sakshi <66660153+Sakshiiii23@users.noreply.github.com>
2020-10-23 15:47:25 +0530

    Create rotateArray.cpp

---
+++ rotateArray.cpp
@@ -0,0 +1,14 @@
+Q.Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        
+        reverse(nums.begin(),nums.begin()+nums.size()-k);
+        reverse(nums.begin()+nums.size()-k,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+    
+    
+};
